fd3b45e8e9a79a05873ea82eae093b81b0691bd2
danielbairamian <daniel.bairamian@mail.mcgill.ca>
2017-04-06 22:08:52 -0400

    final tilo , fixed error in HomePage

--- Tile-O/src/ca/mcgill/ecse223/tileo/view/HomePage.java
+++ Tile-O/src/ca/mcgill/ecse223/tileo/view/HomePage.java
@@ -18,7 +18,7 @@ import ca.mcgill.ecse223.tileo.model.Game;
 import ca.mcgill.ecse223.tileo.model.TileO;
 
 public class HomePage extends JFrame {
-	
+
 	//UI elements
 	private JLabel welcomeMessage;
 	private JButton gotoCreateGame;
@@ -34,8 +34,8 @@ public class HomePage extends JFrame {
 	private String error;
 	private TileO tileO;
 
-	
-	
+
+
 	public HomePage(TileO tileO){
 		this.tileO = tileO;
 		initComponents();
@@ -43,82 +43,82 @@ public class HomePage extends JFrame {
 	}
 
 	public void initComponents(){
-		
-	//set the size of the applet
-	this.setPreferredSize(new Dimension(1400, 700) );
-		
-	//initialize all the elements
-	welcomeMessage = new JLabel();
-	
-	welcomeMessage.setText("Welcome to Group11's Tile-O !");
-	
-	welcomeMessage.setFont(new Font("Serif", Font.BOLD, 100));
-	
-	
-	
-	gotoCreateGame = new JButton();
-	gotoCreateGame.setPreferredSize(new Dimension(200 , 700));
-	gotoCreateGame.setText("Create a new game");
-	
-	gotoLoadedGame = new JButton();
-	gotoLoadedGame.setText("Load Playmode game");
-	
-	gotoLoadedGameDesign = new JButton();
-	gotoLoadedGameDesign.setText("Load Designmode game");
-	
-	listOfGames = new JComboBox<Integer>();
-	listOfGamesDesign = new JComboBox<Integer>();
-	
-	availableGames = new HashMap<Integer, Game>();
-	availableGamesDesign = new HashMap<Integer, Game>();
-	
-	errorLabel = new JLabel();
-	errorLabel.setText("");
-	
-	gotoCreateGame.addActionListener(new java.awt.event.ActionListener(){
-		public void actionPerformed(java.awt.event.ActionEvent evt){
-			creatNewGame();
-		}
-	});
-	
-	gotoLoadedGame.addActionListener(new java.awt.event.ActionListener(){
-		public void actionPerformed(java.awt.event.ActionEvent evt){
-			loadGame();
-		}
-	});
-	
-	gotoLoadedGameDesign.addActionListener(new java.awt.event.ActionListener(){
-		public void actionPerformed(java.awt.event.ActionEvent evt){
-			loadGameDesign();
-		}
-	});
-	
-	listOfGames.addActionListener(new java.awt.event.ActionListener() {
-		public void actionPerformed(java.awt.event.ActionEvent evt) {
-			JComboBox<Integer> cb = (JComboBox<Integer>) evt.getSource();
-			if(cb.getSelectedItem() != null){
-			selectedGame = (int) cb.getSelectedItem();
-			}
-		}
-	});
-	
-	listOfGamesDesign.addActionListener(new java.awt.event.ActionListener() {
-		public void actionPerformed(java.awt.event.ActionEvent evt) {
-			JComboBox<Integer> cb = (JComboBox<Integer>) evt.getSource();
-			if(cb.getSelectedItem() != null){
-			selectedGameDesign = (int) cb.getSelectedItem();
-			}
-		}
-	});
 
-	
-	GroupLayout layout = new GroupLayout(getContentPane());
-	getContentPane().setLayout(layout);
-	layout.setAutoCreateGaps(true);
-	layout.setAutoCreateContainerGaps(true);
-	
-	layout.setHorizontalGroup(
-			layout.createParallelGroup()
+		//set the size of the applet
+		this.setPreferredSize(new Dimension(1400, 700) );
+
+		//initialize all the elements
+		welcomeMessage = new JLabel();
+
+		welcomeMessage.setText("Welcome to Group11's Tile-O !");
+
+		welcomeMessage.setFont(new Font("Serif", Font.BOLD, 100));
+
+
+
+		gotoCreateGame = new JButton();
+		gotoCreateGame.setPreferredSize(new Dimension(200 , 700));
+		gotoCreateGame.setText("Create a new game");
+
+		gotoLoadedGame = new JButton();
+		gotoLoadedGame.setText("Load Playmode game");
+
+		gotoLoadedGameDesign = new JButton();
+		gotoLoadedGameDesign.setText("Load Designmode game");
+
+		listOfGames = new JComboBox<Integer>();
+		listOfGamesDesign = new JComboBox<Integer>();
+
+		availableGames = new HashMap<Integer, Game>();
+		availableGamesDesign = new HashMap<Integer, Game>();
+
+		errorLabel = new JLabel();
+		errorLabel.setText("");
+
+		gotoCreateGame.addActionListener(new java.awt.event.ActionListener(){
+			public void actionPerformed(java.awt.event.ActionEvent evt){
+				creatNewGame();
+			}
+		});
+
+		gotoLoadedGame.addActionListener(new java.awt.event.ActionListener(){
+			public void actionPerformed(java.awt.event.ActionEvent evt){
+				loadGame();
+			}
+		});
+
+		gotoLoadedGameDesign.addActionListener(new java.awt.event.ActionListener(){
+			public void actionPerformed(java.awt.event.ActionEvent evt){
+				loadGameDesign();
+			}
+		});
+
+		listOfGames.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
+				JComboBox<Integer> cb = (JComboBox<Integer>) evt.getSource();
+				if(cb.getSelectedItem() != null){
+					selectedGame = (int) cb.getSelectedItem();
+				}
+			}
+		});
+
+		listOfGamesDesign.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
+				JComboBox<Integer> cb = (JComboBox<Integer>) evt.getSource();
+				if(cb.getSelectedItem() != null){
+					selectedGameDesign = (int) cb.getSelectedItem();
+				}
+			}
+		});
+
+
+		GroupLayout layout = new GroupLayout(getContentPane());
+		getContentPane().setLayout(layout);
+		layout.setAutoCreateGaps(true);
+		layout.setAutoCreateContainerGaps(true);
+
+		layout.setHorizontalGroup(
+				layout.createParallelGroup()
 				.addComponent(welcomeMessage)
 				.addComponent(errorLabel)
 				.addGroup(layout.createSequentialGroup()
@@ -132,20 +132,20 @@ public class HomePage extends JFrame {
 								.addComponent(gotoLoadedGameDesign)
 								)
 
-			
-	
+
+
 						)
-		
+
 				);
-	 
-	
-	layout.linkSize(SwingConstants.HORIZONTAL, new java.awt.Component[] {gotoLoadedGame, listOfGames , listOfGamesDesign , gotoLoadedGameDesign});
-	//layout.linkSize(SwingConstants.HORIZONTAL, new java.awt.Component[] {MoveTo, removeConnection});
-	layout.linkSize(SwingConstants.VERTICAL, new java.awt.Component[] {gotoLoadedGame, listOfGames , listOfGamesDesign , gotoLoadedGameDesign});
-	//layout.linkSize(SwingConstants.HORIZONTAL, new java.awt.Component[] {connectTiles, removeConnection});
-		
-	layout.setVerticalGroup(
-			layout.createSequentialGroup()
+
+
+		layout.linkSize(SwingConstants.HORIZONTAL, new java.awt.Component[] {gotoLoadedGame, listOfGames , listOfGamesDesign , gotoLoadedGameDesign});
+		//layout.linkSize(SwingConstants.HORIZONTAL, new java.awt.Component[] {MoveTo, removeConnection});
+		layout.linkSize(SwingConstants.VERTICAL, new java.awt.Component[] {gotoLoadedGame, listOfGames , listOfGamesDesign , gotoLoadedGameDesign});
+		//layout.linkSize(SwingConstants.HORIZONTAL, new java.awt.Component[] {connectTiles, removeConnection});
+
+		layout.setVerticalGroup(
+				layout.createSequentialGroup()
 				.addComponent(welcomeMessage)
 				.addComponent(errorLabel)
 				.addGroup(layout.createParallelGroup()
@@ -153,57 +153,57 @@ public class HomePage extends JFrame {
 						.addGroup(layout.createSequentialGroup()
 								.addComponent(listOfGames)
 								.addComponent(gotoLoadedGame)
-							
-								
-								
-								
+
+
+
+
 								)
 						.addGroup(layout.createSequentialGroup()
 								.addComponent(listOfGamesDesign)
 								.addComponent(gotoLoadedGameDesign)
-							
-								
-								
-								
+
+
+
+
 								)
-						
-						
+
+
 						)
-			
-			
-			
-			);
-	
-	pack();
-		
+
+
+
+				);
+
+		pack();
+
 	}
-	
+
 	private void refreshData(){
 		errorLabel.setText(error);
-		
+
 		PlayController pc = new PlayController(tileO);
 		int index = 0;
 		int indextwo = 0;
 		listOfGames.removeAllItems();
 		for(Game game : pc.getGames()){
 			if(game.getMode()!= null){
-			if(!(game.getMode().equals(Game.Mode.DESIGN))){
-			availableGames.put(index, game);
-			listOfGames.addItem(game.getTileO().indexOfGame(game));
-			
-			}
+				if(!(game.getMode().equals(Game.Mode.DESIGN))){
+					availableGames.put(index, game);
+					listOfGames.addItem(game.getTileO().indexOfGame(game));
+
+				}
 			}
 			index++;
 		}
-		
+
 		for(Game game : pc.getGames()){
 			if(game.getMode()!= null){
-			if((game.getMode().equals(Game.Mode.DESIGN))){
-			availableGamesDesign.put(indextwo, game);
-			listOfGamesDesign.addItem(game.getTileO().indexOfGame(game));
-			indextwo++;
+				if((game.getMode().equals(Game.Mode.DESIGN))){
+					availableGamesDesign.put(indextwo, game);
+					listOfGamesDesign.addItem(game.getTileO().indexOfGame(game));
+					indextwo++;
+				}
 			}
-		}
 		}
 		selectedGame = -1;
 		selectedGameDesign =-1;
@@ -211,35 +211,43 @@ public class HomePage extends JFrame {
 		listOfGamesDesign.setSelectedIndex(selectedGameDesign);
 		error = "";
 	}
-	
+
 	public void creatNewGame(){
 		//redirect to the create game page
 		dispose();
 		new DesignTileOPage(tileO,false).setVisible(true);
 	}
-	
-	
+
+
 	public void loadGame(){
-		System.out.println(selectedGame);
-		PlayController pc = new PlayController(tileO);
-		Game game = TileOApplication.getGame(selectedGame);
-		tileO.setCurrentGame(game);
-		try {
-			pc.startGame(game);
-			dispose();
-			new PlayTileOPage(tileO, pc).setVisible(true);
-		} catch (InvalidInputException e) {
-			error=e.getMessage();
+		if(selectedGame == -1){
+			return;
+		} else{
+			System.out.println(selectedGame);
+			PlayController pc = new PlayController(tileO);
+			Game game = TileOApplication.getGame(selectedGame);
+			tileO.setCurrentGame(game);
+			try {
+				pc.startGame(game);
+				dispose();
+				new PlayTileOPage(tileO, pc).setVisible(true);
+			} catch (InvalidInputException e) {
+				error=e.getMessage();
+			}
 		}
 	}
-	
+
 	public void loadGameDesign(){
-		Game game = TileOApplication.getGame(selectedGameDesign);
-		tileO.setCurrentGame(game);
+		if(selectedGameDesign == -1){
+			return;
+		} else{
+			Game game = TileOApplication.getGame(selectedGameDesign);
+			tileO.setCurrentGame(game);
 			dispose();
 			new DesignTileOPage(game , tileO, true).setVisible(true);
-		
+		}
+
 	}
 
-	
+
 }
